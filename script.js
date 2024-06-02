65368000f35a4e839e983abc6f86ecf017cac9dd
kirill-mrotzek <163567376+kirill-mrotzek@users.noreply.github.com>
2024-06-02 19:22:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,42 @@
-let menu = {
-    width: 200, 
-    height: 300,
-    title: "my menu"
-};
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for(let key in menu){
-        if(typeof (menu[key]) == 'number'){
-            menu[key]*=2;
-        }
+for (let i = 0; i < num.length; i++){
+            console.log(num[i]);
+}
+
+let numbers = [-3, 16, 8, -34, 65, 120, 42, 15, 7, -4, -7, 3, 46, 26, 83];
+
+for(let a = 0; a < numbers.length; a++){
+    if(numbers[a]>=15){
+        console.log(numbers[a]);
     }
-    console.log(menu);
+}
 
-    let arr = [
-        {
-        id: 1,
-        title: "bicycle",
-        price: 45000,
-        discount: 10
-        },
-        {
-        id: 2,
-        title: "roller-skates",
-        price: 15000,
-        discount: 5
-        },
-        {
-        id: 3,
-        title: "Kick scooter",
-        price: 10000,
-        discount: 30
-        },
-        {
-        id: 4,
-        title: "skis",
-        price: 25000,
-        discount: 20
-        },
-        {
-        id: 5,
-        title: "skate",
-        price: 10000,
-        discount: 0
+for(let b = 0; b < numbers.length; b++){
+    if(numbers[b]%2 !=0){
+    console.log(numbers[b]);
     }
-];
-
-for(let i = 0; i < arr.length; i++){
-    console.log(arr[i].title);
 }
 
-for(let i = 0; i < arr.length; i++){
-    console.log(`${arr[i].title} (${arr[i].price})`);
+for(let c = 3; c < numbers.length; c+=3){
+    console.log(numbers[c]);
 }
 
-for(let i = 0; i < arr.length; i++){
-    let discountedPrice = arr[i].price - arr[i].discount;
-    console.log(`${arr[i].title} (${discountedPrice})`);
+let sum = 0;
+for(let d = 0; d < numbers.length; d++){
+    if(numbers[d]%2 !=0)
+    sum=sum+numbers[d];
 }
+console.log(sum);
+
+
+let sumEven = 0;
+let sumOdd = 0;
+
+    for(let e = 0; e < numbers.length; e++){
+    if(numbers[e]%2 !=0){
+        sumOdd=sumOdd+numbers[e];
+    }else if(numbers[e]%2 ==0)
+        sumEven=sumEven+numbers[e];
+    }
+console.log(sumEven - sumOdd);
